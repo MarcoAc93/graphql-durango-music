97730274d4d3b1f48a3615a56619b8b5ba1e1757
Marco Andrade <alonso_ac93@hotmail.com>
2024-06-06 15:56:26 -0600

    authUser query

--- src/schema/users/resolver/index.ts
+++ src/schema/users/resolver/index.ts
@@ -1,12 +1,27 @@
 import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import UserModel from '../../../models/Users';
 
-import UserModel from "../../../models/Users";
+const generateToken = (user: any, secretKey: string, options?: jwt.SignOptions) => {
+  const { id, email, name, lastName } = user;
+  return jwt.sign({ id, email, name, lastName }, secretKey, options);
+};
 
 const resolvers = {
   Query: {
     getUser: async () => {
       const user = await UserModel.find({});
       return user;
+    },
+    authUser: async (_: any, { input }: { input: any }) => {
+      const { email, password } = input;
+      const userExists = await UserModel.findOne({ email });
+      if (!userExists) throw new Error('El usuario no existe'); 
+
+      const correctPassword = await bcrypt.compare(password, userExists.password);
+      if (!correctPassword) throw new Error('Credenciales incorrectas');
+      const token = generateToken(userExists, process.env.SECRET_KEY ?? '', { expiresIn: '24h' })
+      return { token };
     }
   },
   Mutation: {
